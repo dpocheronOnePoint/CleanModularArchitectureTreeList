bb46b6a4cd2fcf48377fc7144b89422caea3b926
Pocheron Dimitri <dimitri.pocheron@external.waykonect.com>
2025-05-26 10:32:42 +0200

    Initialize project

---
+++ CleanModularArchitectureTreeList/CleanModularArchitectureTreeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanModularArchitectureTreeListApp.swift
+//  CleanModularArchitectureTreeList
+//
+//  Created by Dimitri POCHERON on 23/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct CleanModularArchitectureTreeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
